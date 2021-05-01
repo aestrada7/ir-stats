9e56d272aed568605ed3da34f95f767d94fb5f4a
Antonio Estrada <antonio.estrada@globant.com>
2021-04-30 20:11:44 -0500

    Version 0.8.3 renamed env

--- services/Config.js
+++ services/Config.js
@@ -3,5 +3,5 @@ export const DATABASE_NEDB = 'NEDB';
 export const DATABASE_LOCAL_STORAGE = 'LOCAL_STORAGE';
 
 //configuration (later move to a json)
-export const SERVER_URL = process.env.NEXT_PUBLIC_URL;
+export const SERVER_URL = `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 export const DATA_PROVIDER = DATABASE_NEDB; //or DATABASE_LOCAL_STORAGE

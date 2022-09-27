ea00ccd9673f1e2dc9991686d8f101963e2b76d9
Antonio Estrada <antonio.estrada@globant.com>
2022-09-27 17:27:19 -0500

    Version 1.0.6 Added filtering for track and week pages too, moved them to functional components

--- pages/track/[...track].js
+++ pages/track/[...track].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 import { raceDataFetchAll, trackDataFetch } from '../../services/DataFetch';
@@ -10,41 +10,45 @@ import Table from '../../components/Table';
 import WeekTable from '../../components/WeekTable';
 import IRatingChart from '../../components/IRatingChart';
 
-class Track extends React.Component {
-    static async getInitialProps({ query }) {
-        const trackId = query.track[0];
-        const hideLegacy = query.track[1] || false;
+import { TableFilterContext } from '../../services/TableFilterContext';
 
-        const trackData = await trackDataFetch();
-        const track = trackData.filter(x => x.id == trackId)[0].name;
-        const legacyTrackIds = !hideLegacy ? trackData.filter(x => x.id == trackId)[0].legacyIds || null : null;
-        let newTrackIds = [];
-        try {
-            newTrackIds.push(trackData.filter(x => x.legacyIds == trackId)[0].id || null);
-        } catch(e) {
-            //do nothing
-        }
+const Track = ({ pageTitle, raceData, trackData, weekData, trackId, legacyTrackIds }) => {
+    const [ filter, setFilter ] = useState({});
+    const value = { filter, setFilter };
 
-        const pageTitle = `Results at ${track}`;
-
-        const raceDataAll = await raceDataFetchAll();
-        const raceData = raceDataAll.filter(x => x.trackid == trackId || itemExists(x.trackid, legacyTrackIds) || itemExists(x.trackid, newTrackIds));
-
-        const weekData = weekDataBuild(raceData);
-        return { pageTitle, raceData, trackData, weekData, trackId, legacyTrackIds, newTrackIds };
-    }
-
-    render() {
-        const { pageTitle, trackData, weekData, raceData, trackId, legacyTrackIds, newTrackIds } = this.props;
-        return (
-            <Layout title={pageTitle} backButton={true}>
+    return (
+        <Layout title={pageTitle} backButton={true}>
+            <TableFilterContext.Provider value={value}>
                 { legacyTrackIds ? <Link href={`/track/${trackId}/1`}><button className="hide-legacy">Hide Legacy Data</button></Link> : '' }
                 <WeekTable weekData={weekData}></WeekTable>
                 <Table raceData={raceData} trackData={trackData} showSeason={true}></Table>
                 <IRatingChart raceData={raceData}></IRatingChart>
-            </Layout>
-        );
+            </TableFilterContext.Provider>
+        </Layout>
+    );
+}
+
+Track.getInitialProps = async({ query }) => {
+    const trackId = query.track[0];
+    const hideLegacy = query.track[1] || false;
+
+    const trackData = await trackDataFetch();
+    const track = trackData.filter(x => x.id == trackId)[0].name;
+    const legacyTrackIds = !hideLegacy ? trackData.filter(x => x.id == trackId)[0].legacyIds || null : null;
+    let newTrackIds = [];
+    try {
+        newTrackIds.push(trackData.filter(x => x.legacyIds == trackId)[0].id || null);
+    } catch(e) {
+        //do nothing
     }
+
+    const pageTitle = `Results at ${track}`;
+
+    const raceDataAll = await raceDataFetchAll();
+    const raceData = raceDataAll.filter(x => x.trackid == trackId || itemExists(x.trackid, legacyTrackIds) || itemExists(x.trackid, newTrackIds));
+
+    const weekData = weekDataBuild(raceData);
+    return { pageTitle, raceData, trackData, weekData, trackId, legacyTrackIds };
 }
 
 export default Track;
--- pages/week/[...week].js
+++ pages/week/[...week].js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { raceDataFetch, trackDataFetch, messageGet } from '../../services/DataFetch';
 import { weekDataBuild } from '../../services/WeekDataStats';
-import * as Config from '../../services/Config';
 
 import Layout from '../../components/Layout';
 import Table from '../../components/Table';
@@ -11,32 +10,37 @@ import ImageCarousel from '../../components/ImageCarousel';
 import IRatingChart from '../../components/IRatingChart';
 import Notes from '../../components/Notes';
 
-class Week extends React.Component {
-    static async getInitialProps({ query }) {
-        const week = query.week[2];
-        const season = query.week[1];
-        const year = query.week[0];
-        const pageTitle = `${year} S${season} Week ${parseInt(week) + 1} Detail`;
-        const raceData = await raceDataFetch(year, season, week);
-        const trackData = await trackDataFetch();
-        const weekData = weekDataBuild(raceData);
-        const car = raceData[0].carid;
-        const notes = await messageGet(year, season, week, car);
-        return { pageTitle, raceData, trackData, weekData, season, year, week, car, notes };
-    }
+import { TableFilterContext } from '../../services/TableFilterContext';
 
-    render() {
-        const { pageTitle, raceData, trackData, weekData, season, year, week, car, notes } = this.props;
-        return (
-            <Layout title={pageTitle} backButton={true}>
+const Week = ({ pageTitle, raceData, trackData, weekData, season, year, week, car, notes }) => {
+    const [ filter, setFilter ] = useState({});
+    const value = { filter, setFilter };
+
+    return(
+        <Layout title={pageTitle} backButton={true}>
+            <TableFilterContext.Provider value={value}>
                 <WeekTable weekData={weekData}></WeekTable>
                 <Table raceData={raceData} trackData={trackData}></Table>
                 <Notes season={season} year={year} week={week} car={car} notes={notes}></Notes>
                 <ImageCarousel season={season} year={year} week={week}></ImageCarousel>
                 <IRatingChart raceData={raceData}></IRatingChart>
-            </Layout>
-        );
-    }
+            </TableFilterContext.Provider>
+        </Layout>
+    )
+}
+
+Week.getInitialProps = async({ query }) => {
+    const week = query.week[2];
+    const season = query.week[1];
+    const year = query.week[0];
+    const pageTitle = `${year} S${season} Week ${parseInt(week) + 1} Detail`;
+    const raceData = await raceDataFetch(year, season, week);
+    const trackData = await trackDataFetch();
+    const weekData = weekDataBuild(raceData);
+    const car = raceData[0].carid;
+    const notes = await messageGet(year, season, week, car);
+
+    return { pageTitle, raceData, trackData, weekData, season, year, week, car, notes };
 }
 
 export default Week;
